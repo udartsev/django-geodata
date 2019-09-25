fb0e875d14801f1f8e9e28ea2beccd3bc76bf838
Vladimir Udartsev <vladimir.udartsev@appcraft.team>
2019-09-25 13:37:03 +0300

    changed .gitignore and licence to MIT

--- setup.py
+++ setup.py
@@ -12,18 +12,19 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
-    license='BSD License',  # example license
+    license='MIT License',
     description='Django app to add GEO data [Countries, Cities, Time Zones, Geo Points and their Polygons in geoJSON format.]',
     long_description=README,
+    long_description_content_type='text/markdown',
     url='https://github.com/udartsev/django-geodata',
     author='Vladimir Udartsev',
     author_email='v.udartsev@gmail.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.2',  # replace "X.Y" as appropriate
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
-        'License :: BSD License',  # example license
+        'License :: Aladdin Free Public License (AFPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
